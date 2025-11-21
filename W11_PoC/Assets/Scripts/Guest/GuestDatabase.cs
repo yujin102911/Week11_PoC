62ab4804e69555f32cbae6d9440cb66a407731f2
박유진 <yujin102911@gmail.com>
2025-11-21 22:03:53 +0900

    난 게스트데이터베이스를 찢어

---
+++ W11_PoC/Assets/Scripts/Guest/GuestDatabase.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGuestDB", menuName = "Game/Guest Database")]
+public class GuestDatabase : ScriptableObject
+{
+    public List<GuestData> guests;
+}
