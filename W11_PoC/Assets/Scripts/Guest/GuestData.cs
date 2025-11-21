85a83f6e8c0bff96b285d7b70f98449479614ea8
박유진 <yujin102911@gmail.com>
2025-11-21 21:28:14 +0900

    손님시스템

---
+++ W11_PoC/Assets/Scripts/Guest/GuestData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewGuest", menuName = "Game/Guest Data")]
+public class GuestData : ScriptableObject
+{
+    [Header("손님 기본 정보")]
+    public string guestID;      // 왔는지 체크용 ID
+    public GameObject guestPrefab; // 손님 외형 프리팹 (없으면 기본값 사용)
+
+    [Header("주문 정보")]
+    public List<BlockData> orderList; // 원하는 블록 리스트
+
+    [Header("조건")]
+    public float patienceTime = 30f; // 기다릴 수 있는 시간
+    public int paymentAmount = 100;  // 지불 금액
+}
+
+[CreateAssetMenu(fileName = "NewGuestDB", menuName = "Game/Guest Database")]
+public class GuestDatabase : ScriptableObject
+{
+    public List<GuestData> guests;
+}
